9da17816881a397176dce7781e66fcc00c425142
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-08-22 21:56:44 -0300

    added structure

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+   production: false,
+   localhostUrl: 'http://localhost:4000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+   production: true,
+   localhostUrl: 'http://localhost:4000'
+};
